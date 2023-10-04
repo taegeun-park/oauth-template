e2af19809ccba31a67db5967f79bd7f8f2566b3c
taegeun-park <taegeun0525@gamil.com>
2023-10-05 00:53:48 +0900

    first commit

---
+++ src/main/java/com/example/oauthsecuritytempalte/OauthSecurityTempalteApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthsecuritytempalte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSecurityTempalteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthSecurityTempalteApplication.class, args);
+    }
+
+}
